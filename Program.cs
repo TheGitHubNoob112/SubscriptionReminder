12eb085c3db15cf80f3ed74b4633d6958559d83e
TheGitHubNoob112 <131225671+TheGitHubNoob112@users.noreply.github.com>
2025-04-21 18:54:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+
+// Your code goes here
+
+if (daysUntilExpiration <= 5) {
+    discountPercentage = 10;
+}
+if (daysUntilExpiration <= 1) {
+    discountPercentage = 20;
+}
+
+if (daysUntilExpiration <= 1){
+    Console.WriteLine($"Your subscription ends within a day, renew now and save {discountPercentage}% ");
+}
+else if  (daysUntilExpiration <=5){
+    Console.WriteLine($"your subscription expires in {daysUntilExpiration} days, renew now and save {discountPercentage}%");
+
+}
+else if (daysUntilExpiration <= 10){
+    Console.WriteLine("Your subscription will expire soon, Renew now!");
+}
+
+
